07c1a9cc2e42e4f1bc254f21014611a069a31620
Néstor <nestorblancotech@gmail.com>
2023-08-29 11:57:43 +0200

    Start

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-library
+A JavaScript project
---
+++ src/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function App() {
+
+  let button = document.getElementById("goButton");
+
+  button.addEventListener("click", ConsoleIt);
+
+  function ConsoleIt(){
+    console.log("asdfñlgjkh")
+  }
+
+}
+
+App();
